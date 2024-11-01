26623651a20bfe696624b9f266c12e060782bf4b
Paulo Cavalcante <pauloc1807@hotmail.com>
2024-11-01 04:24:18 -0300

    Inserindo Operações Aritméticas básicas nesse pequeno programa e podendo acessá-las pelo switch

--- programa001paulo.c
+++ programa001paulo.c
@@ -8,8 +8,9 @@
 
 //variáveis globais//
 float real, dolar, euro, libra, realConvert, dolarConvert, euroConvert, libraConvert;
-int escolha, inicialEsc;
-
+int escFinanceira, inicialEsc,escCalculadora;
+int soma, sub, multi,division, numero1, numero2, continuarOperacao, numeroProx, resultado, resto;
+int fecharOuNao;
 //Protótipo das funções//
 void convertDolarToReal();
 void convertDolarToEuro();
@@ -25,8 +26,11 @@ void convertLibraToReal();
 void convertLibraToEuro();
 void menuInicial();
 void menuFinanceiro();
+void menuCalculadora();
 void escolhaInicial(int inicialEsc);
-void escolhaFinanceiro(int escolha);
+void escolhaFinanceiro(int escFinanceira);
+void escolhaCalculadora(int escCalculadora);
+void funcionalidades();
 
 //functions//
 void convertDolarToReal(){
@@ -37,7 +41,6 @@ void convertDolarToReal(){
     realConvert = (dolar * 5.7116);
     printf("\n\n%.2f dólares são %.2fR$ *;", dolar, realConvert);
     printf("\n\n*Valores atualizados do dia 29/10/2024, Terça-Feira.\n\n");
-    exit(0);
 }
 void convertDolarToEuro(){
     printf("Insira a quantia de dólares que quer converter em euros: \n");
@@ -47,7 +50,6 @@ void convertDolarToEuro(){
         euroConvert = (dolar * 0.9252);
     printf("\n\n%.2f dólares são %.2f€ *", dolar, euroConvert);
     printf("\n\n*Valores atualizados do dia 29/10/2024, Terça-Feira.\n\n");
-    exit(0);
 }
 void convertDolarToLibra(){
     printf("Insira a quantia de dólares que quer converter em libras esterlinas: \n");
@@ -57,7 +59,6 @@ void convertDolarToLibra(){
         libraConvert = (dolar * 0.7716);
     printf("\n\n%.2f dólares são %.2f£ libras *", dolar, libraConvert);
     printf("\n\n*Valores atualizados do dia 29/10/2024, Terça-Feira.\n\n");
-    exit(0);
 }                                       
 void convertRealToDolar(){
     printf("Insira a quantia de reais que quer converter em dólares: \n");
@@ -76,7 +77,6 @@ void convertRealToEuro(){
         euroConvert = (real * 0.1620);
     printf("\n\n%.2f reais são %.2f€ *;", real, euroConvert);
     printf("\n\n*Valores atualizados do dia 29/10/2024, Terça-feira");
-    exit(0);
 }
 void convertRealToLibra(){
     printf("Insira a quantia de reais que quer converter em libras: \n");
@@ -86,7 +86,6 @@ void convertRealToLibra(){
         libraConvert = (real * 0.1351);
     printf("\n\n%.2f reais são %.2f£ *;", real, libraConvert);
     printf("\n\n*Valores atualizados do dia 29/10/2024, Terça-feira");
-    exit(0);
 }
 void convertEuroToDolar(){
     printf("Insira a quantia de euros que quer converter em dólares: \n");
@@ -106,7 +105,6 @@ void convertEuroToReal(){
         realConvert = (euro * 6.1753);
     printf("\n\n%.2f euros são %.2fR$ *", euro, realConvert);
     printf("\n*Valores atualizados do dia 29/10/2024, Terça-feira");
-    exit(0);
 }
 void convertEuroToLibra(){
     printf("Insira a quantia de euros que quer converter em libras: \n");
@@ -116,7 +114,6 @@ void convertEuroToLibra(){
         libraConvert = (euro * 0.8340);
     printf("\n\n%.2f euros são %.2f£ *", euro, libraConvert);
     printf("\n\n*Valores atualizados do dia 29/10/2024, Terça-feira");
-    exit(0);
 }
 void convertLibraToDolar(){
     printf("Insira a quantia de libras que quer converter em dólares: \n");
@@ -135,7 +132,6 @@ void convertLibraToReal(){
         realConvert = (libra * 7.4019);
     printf("\n\n%.2f libras são %.2fR$ *", libra, realConvert);
     printf("\n*Valores atualizados do dia 29/10/2024, Terça-feira");
-    exit(0);
 }
 void convertLibraToEuro(){
     printf("Insira a quantia de libras que quer converter em euros: \n");
@@ -145,7 +141,93 @@ void convertLibraToEuro(){
         euroConvert = (libra * 1.1992);
     printf("\n\n%.2f libras são %.2f€ *", libra, euroConvert);
     printf("\n\n*Valores atualizados do dia 29/10/2024, Terça-feira");
-    exit(0);
+}
+
+void adicao(){
+    printf("Insira o primeiro número da operação: \n");
+    scanf("%d",  &numero1);
+    printf("\nInsira o segundo número que quer somar: ");
+    scanf("%d",&numero2);
+    soma = (numero1 + numero2);
+    printf("O resultado da soma entre os dois números é: %d\n", soma);
+}
+   /*// Preciso adicionar depois//
+    while(0){
+        printf("\nQuer inserir mais um número? \n");
+        scanf("%i", &continuarOperacao);
+        if(continuarOperacao == 'n' || continuarOperacao == 'N'){
+            break;
+        }
+    }
+    if (continuarOperacao == 'S' && continuarOperacao == 's'){
+        printf("Insira o próximo número: ");
+        scanf("%d", &numeroProx);
+        resultado = (soma + numeroProx);
+         printf("O resultado mais o número adicionado é: %d\n", resultado);
+    }
+    printf("Quer inserir mais um número? \n"); printf("\nQuer inserir mais um número? \n");*/
+
+void subtracao(){
+    printf("Insira o primeiro número da operação: \n");
+    scanf("%d",  &numero1);
+    printf("\nInsira o segundo número da operação: \n");
+    scanf("%d",&numero2);
+    sub = (numero1 - numero2);
+    printf("O resultado da subtracão é: %d - %d = %d\n",numero1,numero2, sub);
+}
+void multiplication(){
+    printf("Insira o primeiro número da operação: \n");
+    scanf("%d",  &numero1);
+    printf("\nInsira o segundo número da operação: \n");
+    scanf("%d",&numero2);
+    multi = (numero1 * numero2);
+    printf("O resultado da multiplicação é: %d x %d = %d",numero1,numero2, multi);
+}
+void divisao(){
+    printf("Insira o primeiro número da operação: \n");
+    scanf("%d",  &numero1);
+    printf("\nInsira o segundo número da operação: \n");
+    scanf("%d",&numero2);
+    division = (numero1 / numero2);
+    resto = (numero1 % numero2);
+    printf("O resultado da divisão é: %d / %d = %d, resto %d\n",numero1,numero2, division, resto);
+}
+/*void endOrNo(int fecharOuNao){
+            printf("\nQuer retornar ao menu inicial ou encerrar o programa?");
+            printf("\nR para retornar ou E para encerrar\n");
+            scanf("%i", &fecharOuNao);
+            if ( fecharOuNao == 'R' ||  fecharOuNao== 'r') {
+                    funcionalidades();
+            }
+            else if ( fecharOuNao == 'E' ||  fecharOuNao == 'e') {
+                    printf("Você selecionou encerrar!");
+                    printf("Encerrando o progarma...");
+            } exit(0);
+}*/
+void escolhaCalculadora(int escCalculadora){
+    switch(escCalculadora){
+        case 1:
+            adicao();
+            exit(0);
+        case 2:
+            subtracao();
+            exit(0);
+        case 3:
+            multiplication();
+            exit(0);
+        case 4:
+            divisao();
+            exit(0);
+        case 5:
+            system("cls");
+            printf("Você retornou ao menu das funcionalidades do programa");
+            funcionalidades();
+            break;
+        case 0:
+            printf("Encerrando o programa...");
+            exit(0);
+            break;
+    }
 }
 void escolhaInicial(int inicialEsc){
     switch(inicialEsc) {
@@ -153,10 +235,7 @@ void escolhaInicial(int inicialEsc){
             menuFinanceiro();
             break;
         case 2:
-            printf("Em desenvolvimento...");
-            getch();
-            printf("Encerrando o programa...");
-            exit (0);
+            menuCalculadora();
             break;
         case 3:
             printf("Em desenvolvimento...");
@@ -174,46 +253,59 @@ void escolhaInicial(int inicialEsc){
             getch();
     }
 }
-void escolhaFinanceiro(int escolha) {
-    switch (escolha) {
+void escolhaFinanceiro(int escFinanceira) {
+    switch (escFinanceira) {
         case 1 :
             convertDolarToReal();
+            exit(0);
     	    break;
         case 2:
             convertDolarToEuro();
+            exit(0);
             break;
         case 3:
             convertDolarToLibra();
+            exit(0);
             break;
         case 4 : 
             convertRealToDolar();
+            exit(0);
             break;
         case 5:
             convertRealToEuro();
+            exit(0);
             break;
         case 6:
             convertRealToLibra();
+            exit(0);
             break;
         case 7:
             convertEuroToDolar();
+            exit(0);
             break;
         case 8:
             convertEuroToReal();
+             exit(0);
             break;
         case 9:
             convertEuroToLibra();
+             exit(0);
             break;
         case 10:
             convertLibraToDolar();
+            exit(0);
             break;
         case 11:
             convertLibraToReal();
+            exit(0);
             break;
         case 12:
             convertLibraToEuro();
+            exit(0);
             break;
         case 13:
-            menuInicial();
+            funcionalidades();
+            exit(0);
             break;
         case 0 :
             printf("Encerrando o programa...");
@@ -226,27 +318,31 @@ void escolhaFinanceiro(int escolha) {
         getch();
     }
     }
+
 //saída da tela e pá//
  void menuInicial(){ 
     printf("Tela de Inicialização do programa em C");
     printf("\nEscolha uma opção a seguir:");
     printf("\nAperte qualquer tecla...\n");
     getch();
-    printf("\n\n1 - Financeiro");
-    printf("\n2 - Em desenvolvimento ");
-    printf("\n3 - Em desenvolvimento ");
-    printf("\n0 - Encerrar o programa \n");
+    funcionalidades();
+ }
+void funcionalidades(){
+    printf("\n\n            1 - Financeiro ");
+    printf("\n            2 - Calculadora simples ");
+    printf("\n            3 - Em desenvolvimento ");
+    printf("\n            0 - Encerrar o programa \n");
     scanf("%i", &inicialEsc);
     escolhaInicial(inicialEsc);
  }
 
 void menuFinanceiro() { 
     system("cls");
-    printf("Seja bem-vindo ao programa de conversão de moedas Real/Dólar/Euro/Libra!!!");
+    printf("Seja bem-vindo(a) ao programa de conversão de moedas Real/Dólar/Euro/Libra!!!");
     printf("\nEscolha uma opção a seguir:");
     printf("\nAperte qualquer tecla...\n");
     getch();
-    printf("\n\n            1. Converter Dólar para Real");
+    printf("\n            1. Converter Dólar para Real");
     printf("\n            2. Converter Dólar para Euro");
     printf("\n            3. Converter Dólar para Libra");
     printf("\n            4. Converter Real para Dólar");
@@ -260,12 +356,29 @@ void menuFinanceiro() {
     printf("\n           12. Converter Libra para Euro");
     printf("\n           13. Retornar ao menu inicial");
     printf("\n           0. Sair do Programa\n");
-    scanf("%i", &escolha);
-    escolhaFinanceiro(escolha);
+    scanf("%i", &escFinanceira);
+    escolhaFinanceiro(escFinanceira);
+}
+
+void menuCalculadora(){
+    system("cls");
+    printf("Seja bem-vindo(a) a calculadora!!!");
+    printf("\nQual operação deseja efetuar: ");
+    printf("\nAperte qualquer tecla...\n");
+    getch();
+    printf("\n\n            1. Soma");
+    printf("\n            2. Subtração");
+    printf("\n            3. Multiplicação");
+    printf("\n            4. Divisão");
+    printf("\n            5. Retornar ao menu inicial.");
+    printf("\n            0. Sair do Programa.\n");
+    scanf("%i", &escCalculadora);
+    escolhaCalculadora(escCalculadora);
 }
 //main executando tudo//
 int main() {
-    while(1) {
+    while(1){
     menuInicial();
-    } return 0;
+    } 
+    return 0;
 }
